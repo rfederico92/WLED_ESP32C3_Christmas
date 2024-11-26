05c28b83d37941643025a9457dfd8182906e0dd6
rfederico92 <rfederico92@gmail.com>
2024-11-26 02:17:55 +0000

    Working test of OLED display. See usermod.cpp for details. Next update will be to update for v2 standard.

--- treeFlash.bat
+++ treeFlash.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
-ECHO "Merry Christmas! Love Ron of FrequencyLabs"
+ECHO Merry Christmas! Love Ron of FrequencyLabs
 esptool erase_flash
-esptool write_flash 0x0000 bootloader.bin
-esptool write_flash 0x8000 partitions.bin
-esptool write_flash 0xe000 "%HOMEDRIVE%%HOMEPATH%\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin"
-esptool write_flash 0x10000 firmware.bin
+esptool write_flash 0x0000 bootloader.bin 0x8000 partitions.bin 0xe000 "%HOMEDRIVE%%HOMEPATH%\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin" 0x10000 firmware.bin
+ECHO Enjoy!
 PAUSE
